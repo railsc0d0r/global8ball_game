79a301cf178ba53641ce484a0e77542d472c3763
stephan <railsc0d0r@gmail.com>
2016-05-19 06:13:14 +0200

    Tells our engine, which assets to precompile. Refs #101

--- lib/global8ball_game/engine.rb
+++ lib/global8ball_game/engine.rb
@@ -1,5 +1,9 @@
 module Global8ballGame
   class Engine < ::Rails::Engine
     isolate_namespace Global8ballGame
+
+    initializer "global8ball_game.assets.precompile" do |app|
+      app.config.assets.precompile += %w( global8ball_game.js global8ball_game/game/*.js )
+    end
   end
 end
