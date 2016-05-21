68419be745e3f718f40f4e0f169e7bc0aca959fd
stephan <railsc0d0r@gmail.com>
2016-05-21 15:02:44 +0200

    Removes precompiling-config from engine. Investigate how to make this work. For now we configure asset-compilation in app. Refs #101

--- lib/global8ball_game/engine.rb
+++ lib/global8ball_game/engine.rb
@@ -1,9 +1,5 @@
 module Global8ballGame
   class Engine < ::Rails::Engine
     isolate_namespace Global8ballGame
-
-    initializer "global8ball_game.assets.precompile" do |app|
-      app.config.assets.precompile += %w( global8ball_game.js global8ball_game/game/*.js )
-    end
   end
 end
