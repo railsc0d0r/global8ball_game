89664714377f7e846ed62637ac4c79f9278dc1e7
stephan <railsc0d0r@gmail.com>
2016-05-21 15:04:27 +0200

    Configures middleware for dummy-app for translations. Refs #101

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'i18n-js'
+
 
 Bundler.require(*Rails.groups)
 require "global8ball_game"
@@ -10,6 +12,11 @@ module Dummy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Tells backend to automatically export translations and i18n-js
+    config.middleware.use I18n::JS::Middleware
+    I18n.available_locales = [ :de, :en, :es, :fr, :hi, :ru, :zh ]
+
   end
 end
 
