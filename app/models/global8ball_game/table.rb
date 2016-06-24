946817ad05840f697dd955dfcb625b225943c4fb
stephan <railsc0d0r@gmail.com>
2016-06-24 06:24:32 +0200

    Implements table as model to provide configuration-info for the table to the game. Refs #66, #118, #119

---
+++ app/models/global8ball_game/table.rb
@@ -0,0 +1,11 @@
+module Global8ballGame
+  class Table < Config
+    @definition = {
+      table: {
+        border_bounce: 0,
+        table_friction: 0,
+        cue_hardness: 0
+      }
+    }
+  end
+end
---
+++ test/models/global8ball_game/table_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+module Global8ballGame
+  class TableTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
