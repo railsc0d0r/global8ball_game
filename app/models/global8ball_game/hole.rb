05e3d08644824026d5b2e3a37699cbe44d2f3a05
stephan <railsc0d0r@gmail.com>
2016-06-24 06:15:48 +0200

    Implements configuration for holes - has to be defined further. @ the mom only zeros. Refs #66, #118, #121

--- app/models/global8ball_game/hole.rb
+++ app/models/global8ball_game/hole.rb
@@ -1,5 +1,39 @@
 module Global8ballGame
   class Hole < Config
-    @definition = {}
+    @definition = {
+      holes:
+        [
+          {
+            x: 0,
+            y: 0,
+            radius: 0
+          },
+          {
+            x: 0,
+            y: 0,
+            radius: 0
+          },
+          {
+            x: 0,
+            y: 0,
+            radius: 0
+          },
+          {
+            x: 0,
+            y: 0,
+            radius: 0
+          },
+          {
+            x: 0,
+            y: 0,
+            radius: 0
+          },
+          {
+            x: 0,
+            y: 0,
+            radius: 0
+          }
+        ]
+    }
   end
 end
