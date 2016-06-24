8c845795a3021c9ef6d6cee2ba643c2f4a56fe06
stephan <railsc0d0r@gmail.com>
2016-06-24 06:46:49 +0200

    Adds callback to generate a config for a game on after initializing model. Refs #66, #118

--- test/dummy/app/models/game.rb
+++ test/dummy/app/models/game.rb
@@ -1,2 +1,11 @@
 class Game < ApplicationRecord
+  after_initialize do |game|
+    config = {}
+
+    config.merge!(Global8ballGame::Table.config)
+    config.merge!(Global8ballGame::Border.config)
+    config.merge!(Global8ballGame::Hole.config)
+
+    game.config = config.to_json
+  end
 end
