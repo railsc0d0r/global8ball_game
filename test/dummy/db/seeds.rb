98f618c2dc86484cf0bdaeb0751b2cd9339fb55c
stephan <railsc0d0r@gmail.com>
2016-06-27 06:14:02 +0200

    Adds seeds for players to dummy-app. Refs #66, #118

---
+++ test/dummy/db/seeds.rb
@@ -0,0 +1,26 @@
+players = []
+
+player_1 = {
+  name: "David",
+  user_id: 1
+}
+
+players << player_1
+
+player_2 = {
+  name: "Goliath",
+  user_id: 2
+}
+
+players << player_2
+
+player_3 = {
+  name: "Somebody else",
+  user_id: 3
+}
+
+players << player_3
+
+players.each do |player|
+  Player.create!(player)
+end
