ce0b02e485677b1f69444f299f3eb6be5975f263
stephan <railsc0d0r@gmail.com>
2016-07-10 07:51:36 +0200

    Refactors holes-config to named hash in backend to conform convention in frontend. Makes more sense here. Refs #66, #118, #121

--- app/models/global8ball_game/hole.rb
+++ app/models/global8ball_game/hole.rb
@@ -2,38 +2,44 @@ module Global8ballGame
   class Hole < Config
     @definition = {
       holes:
-        [
-          {
-            x: -1.3282,
-            y: -0.6932,
-            radius: 0.0582
-          },
-          {
-            x: -1.3282,
-            y: 0.6932,
-            radius: 0.0582
-          },
-          {
-            x: 0,
-            y: 0.6932,
-            radius: 0.0582
-          },
-          {
-            x: 1.3282,
-            y: 0.6932,
-            radius: 0.0582
-          },
-          {
-            x: 1.3282,
-            y: -0.6932,
-            radius: 0.0582
-          },
-          {
-            x: 0,
-            y: -0.6932,
-            radius: 0.0582
-          }
-        ]
+        {
+          leftTop:
+            {
+              x: -1.3282,
+              y: -0.6932,
+              radius: 0.0582
+            },
+          leftBottom:
+            {
+              x: -1.3282,
+              y: 0.6932,
+              radius: 0.0582
+            },
+          centerBottom:
+            {
+              x: 0,
+              y: 0.6932,
+              radius: 0.0582
+            },
+          rightBottom:
+            {
+              x: 1.3282,
+              y: 0.6932,
+              radius: 0.0582
+            },
+          rightTop:
+            {
+              x: 1.3282,
+              y: -0.6932,
+              radius: 0.0582
+            },
+          centerTop:
+            {
+              x: 0,
+              y: -0.6932,
+              radius: 0.0582
+            }
+        }
     }
   end
 end
