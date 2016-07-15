9cc068e5641be6e8e241075574fb372253d0e839
stephan <railsc0d0r@gmail.com>
2016-07-15 11:07:45 +0200

    Adds method to ApplicationHelper to generate dropdown to select current_viewer. Refs #66, #118, #123

--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def current_viewer_select
+    players = Player.all
+
+    html = "<select id='current_viewer'>".html_safe
+
+    players.each do | player |
+      unless player == players.first
+        html.safe_concat "  <option value='#{player.id}, #{player.name}'>#{player.name}</option>"
+      else
+        html.safe_concat "  <option value='#{player.id}, #{player.name}' selected>#{player.name}</option>"
+      end
+    end
+
+    html.safe_concat "</select>"
+
+    html
+  end
 end
