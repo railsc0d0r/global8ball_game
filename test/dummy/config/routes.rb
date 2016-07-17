74335257edfc5a90f1a59de25793e07a16425b02
stephan <railsc0d0r@gmail.com>
2016-07-17 19:31:17 +0200

    Mounts websocket-server at /cable so it can be accessed at ws://localhost:3000/cable. Refs #66, #92, #93

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # use this route only to simplify development of the game
   get 'game' => 'games#show', as: :game
 
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   root 'games#show'
 end
