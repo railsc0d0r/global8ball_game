4b52f10d511fbfd8586f0b28ecafd2b7d68bff77
stephan <railsc0d0r@gmail.com>
2016-07-19 13:42:55 +0200

    Sets websockets-configuration and implements them in template. Refs #66, #92, #93

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Set the config for websockets
+  # config.action_cable.url = "ws://localhost:3000/cable"
+  # config.action_cable.allowed_request_origins = ['http://localhost:3000']
 end
