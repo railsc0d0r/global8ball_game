7203326bb0e44928c47759c5785fb7ad6dea8c7d
stephan <railsc0d0r@gmail.com>
2016-07-23 03:54:34 +0200

    Lets getState initalize a stage by given data from frontend. Returns the state as json. TODO: move this into a seperate method for initializing and get the last result first. Refs #66, #92, #94

--- test/dummy/app/channels/events_channel.rb
+++ test/dummy/app/channels/events_channel.rb
@@ -2,9 +2,44 @@
 class EventsChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    @game = Game.find(params[:game_id])
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def getState data
+    stage_name = data['current_stage']
+
+    state = {
+        current_stage: {
+            stage_name: stage_name,
+            round: 0
+          }
+      }
+
+    state.merge!(Global8ballGame::BallPosition.config stage_name)
+
+    current_players = {
+      current_players: []
+    }
+
+    if stage_name == 'PlayForVictory'
+      breaker = data['current_breaker']
+      state[:balls][0][:owner] = breaker
+      current_players[:current_players] << { user_id: breaker}
+    end
+
+    if stage_name == 'PlayForBegin'
+      state[:balls][0][:owner] = @game.player_1.id
+      state[:balls][1][:owner] = @game.player_2.id
+      current_players[:current_players] << { user_id: @game.player_1.id}
+      current_players[:current_players] << { user_id: @game.player_2.id}
+    end
+
+    state.merge!(current_players)
+
+    transmit state.to_json
   end
 end
