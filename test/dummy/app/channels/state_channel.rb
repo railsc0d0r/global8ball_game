3067dce1dfc9f403233d98c278f0e7764549f249
stephan <railsc0d0r@gmail.com>
2016-07-24 15:47:46 +0200

    Moves initializing of a stage to a private method. Refs #66, #92

--- test/dummy/app/channels/state_channel.rb
+++ test/dummy/app/channels/state_channel.rb
@@ -14,61 +14,69 @@ class StateChannel < ApplicationCable::Channel
       stage_name = data['current_stage']
       breaker = data['current_breaker']
 
-      state = {
-          current_stage: {
-              stage_name: stage_name,
-              round: 1
-            }
-        }
-
-      state.merge!(Global8ballGame::BallPosition.config stage_name)
-
-      current_players = {
-        current_players: []
-      }
-
-      current_results = {
-        current_results: []
-      }
-
-      if stage_name == 'PlayForBegin'
-        state[:balls][0][:owner] = @game.player_1.id
-        state[:balls][1][:owner] = @game.player_2.id
-        current_players[:current_players] << { user_id: @game.player_1.id}
-        current_players[:current_players] << { user_id: @game.player_2.id}
-      end
-
-      if stage_name == 'PlayForVictory'
-        state[:balls][0][:owner] = breaker
-        current_players[:current_players] << { user_id: breaker}
-        current_results[:current_results] << {
-          stage_name: 'PlayForBegin',
-          winner: breaker
-        }
-      end
-
-      state.merge!(current_players)
-
-      if stage_name == 'ShowResult'
-        current_results[:current_results] << {
-          stage_name: 'PlayForBegin',
-          winner: breaker
-        }
-
-        1.upto 3 do |round|
-          current_results[:current_results] << {
-            stage_name: 'PlayForVictory',
-            round: round,
-            winner: breaker
-          }
-        end
-      end
-
-      state.merge!(current_results)
+      state = initialize_state stage_name, breaker
     else
       state = @game.results.last.result_set
     end
 
     transmit state.to_json
+  end
+
+  private
+
+  def initialize_state stage_name, breaker
+    state = {
+        current_stage: {
+            stage_name: stage_name,
+            round: 1
+          }
+      }
+
+    state.merge!(Global8ballGame::BallPosition.config stage_name)
+
+    current_players = {
+      current_players: []
+    }
+
+    current_results = {
+      current_results: []
+    }
+
+    if stage_name == 'PlayForBegin'
+      state[:balls][0][:owner] = @game.player_1.id
+      state[:balls][1][:owner] = @game.player_2.id
+      current_players[:current_players] << { user_id: @game.player_1.id}
+      current_players[:current_players] << { user_id: @game.player_2.id}
+    end
+
+    if stage_name == 'PlayForVictory'
+      state[:balls][0][:owner] = breaker
+      current_players[:current_players] << { user_id: breaker}
+      current_results[:current_results] << {
+        stage_name: 'PlayForBegin',
+        winner: breaker
+      }
+    end
+
+    state.merge!(current_players)
+
+    if stage_name == 'ShowResult'
+      current_results[:current_results] << {
+        stage_name: 'PlayForBegin',
+        winner: breaker
+      }
+
+      1.upto 3 do |round|
+        current_results[:current_results] << {
+          stage_name: 'PlayForVictory',
+          round: round,
+          winner: breaker
+        }
+      end
+    end
+
+    state.merge!(current_results)
+
+    state
   end
 end
