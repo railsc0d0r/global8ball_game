7e63e965d4f035300d2acfc6b41055a124337347
stephan <railsc0d0r@gmail.com>
2016-07-24 16:58:45 +0200

    Adds route to reset the game. Refs #66, #92

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # use this route only to simplify development of the game
+  # use these routes only to simplify development of the game
   get 'game' => 'games#show', as: :game
+  get 'reset' => 'games#reset_results', as: :reset
 
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
