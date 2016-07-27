0226bf60854b826b7b96192bb4e885236ff52a1c
stephan <railsc0d0r@gmail.com>
2016-07-27 07:08:03 +0200

    Lets state_channel stream from parametrized stream to broadcast states. Refs #66, #110

--- test/dummy/app/channels/state_channel.rb
+++ test/dummy/app/channels/state_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class StateChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "state_#{params[:game_id]}"
     @game = Game.find(params[:game_id])
   end
 
