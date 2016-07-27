8c108e05f4acbddbce418eadcd052f009ad2392b
stephan <railsc0d0r@gmail.com>
2016-07-27 07:09:50 +0200

    Lets shot_channel stream from parametrized stream to broadcast shots. Rebroadcasts shots received from frontend via stream. Moah2come. Refs #66, #110

--- test/dummy/app/channels/shot_channel.rb
+++ test/dummy/app/channels/shot_channel.rb
@@ -1,10 +1,14 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ShotChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "shot_#{params[:game_id]}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def setShot shot
+    ActionCable.server.broadcast "shot_#{params[:game_id]}", shot
   end
 end
