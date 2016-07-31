73ae6686e082f31fb7c70aef74793d77974e571b
stephan <railsc0d0r@gmail.com>
2016-07-31 07:21:20 +0200

    Includes Global8ballGame::PhysicsConcern. Removes stuff implemented by included concern.

--- test/dummy/app/models/game.rb
+++ test/dummy/app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
+  include Global8ballGame::PhysicsConcern
+
   has_many :results, dependent: :destroy
 
   after_initialize do |game|
@@ -16,11 +18,7 @@ class Game < ApplicationRecord
       }
     }
 
-    config = {}
-
-    config.merge!(Global8ballGame::Table.new.config)
-    config.merge!(Global8ballGame::Border.new.config)
-    config.merge!(Global8ballGame::Hole.new.config)
+    config = game.new_table_config
     config.merge!(players_config)
 
     game.config = config.to_json
@@ -47,60 +45,8 @@ class Game < ApplicationRecord
   end
 
   def initialize_state stage_name, breaker
-    state = {
-        current_stage: {
-            stage_name: stage_name,
-            round: 1
-          }
-      }
-
-    state.merge!(Global8ballGame::BallPosition.config stage_name)
-
-    current_players = {
-      current_players: []
-    }
-
-    current_results = {
-      current_results: []
-    }
-
-    if stage_name == 'PlayForBegin'
-      state[:balls][0][:owner] = self.player_1.id
-      state[:balls][1][:owner] = self.player_2.id
-      current_players[:current_players] << { user_id: self.player_1.id}
-      current_players[:current_players] << { user_id: self.player_2.id}
-    end
-
-    if stage_name == 'PlayForVictory'
-      state[:balls][0][:owner] = breaker
-      current_players[:current_players] << { user_id: breaker}
-      current_results[:current_results] << {
-        stage_name: 'PlayForBegin',
-        winner: breaker
-      }
-    end
-
-    state.merge!(current_players)
-
-    if stage_name == 'ShowResult'
-      current_results[:current_results] << {
-        stage_name: 'PlayForBegin',
-        winner: breaker
-      }
-
-      1.upto 3 do |round|
-        current_results[:current_results] << {
-          stage_name: 'PlayForVictory',
-          round: round,
-          winner: breaker
-        }
-      end
-    end
-
-    state.merge!(current_results)
-
     result = Result.new
-    result.result_set=state
+    result.result_set = initial_state self.player_1_id, self.player_2_id, stage_name, breaker
 
     self.results << result
     self.save!
