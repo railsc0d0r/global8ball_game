42cc537228190463d2a07c4e118b8b1beb7a9cec
stephan <railsc0d0r@gmail.com>
2016-09-04 06:34:20 +0200

    Specifies HoleConfig. Refs #66, #92

--- spec/models/global8ball_game/hole_config_spec.rb
+++ spec/models/global8ball_game/hole_config_spec.rb
@@ -2,6 +2,36 @@ require 'rails_helper'
 
 module Global8ballGame
   RSpec.describe HoleConfig, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    before do
+      @hole_config = Global8ballGame::HoleConfig.new
+      @config = @hole_config.config[:holes]
+    end
+
+    it "provides a config-snippet as a hash w/ a key 'holes' and another hash of hashes as value" do
+      expect(@config).to_not be_nil
+      expect(@config).to be_an_instance_of Hash
+    end
+
+    it "provides the same config-snippet as a json-string" do
+      expect(@hole_config.config_json).to be_an_instance_of String
+      expect(JSON.parse(@hole_config.config_json).deep_symbolize_keys).to eq @hole_config.config
+    end
+
+    it "provides 6 hashes to describe the holes" do
+      expect(@config.keys.count).to eq 6
+      @config.keys.each do |hole|
+        expect(@config[hole]).to be_an_instance_of Hash
+      end
+    end
+
+    it "describes each hole w/ numeric values for x- and y-position and the radius" do
+      @config.keys.each do |hole|
+        hole_keys = [:x, :y, :radius]
+        expect(@config[hole].keys).to match_array hole_keys
+        hole_keys.each do |hole_key|
+          expect(@config[hole][hole_key]).to be_a_kind_of Numeric
+        end
+      end
+    end
   end
 end
