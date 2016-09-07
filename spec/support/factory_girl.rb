908319734927ebf3c98415021dc08800c9f431e7
stephan <railsc0d0r@gmail.com>
2016-09-07 23:09:11 +0200

    Adds initializers for factory_girl and database_cleaner to specs. Refs #66, #92

---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,10 @@
+# RSpec without Rails
+require 'factory_girl'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryGirl.find_definitions
+  end
+end
