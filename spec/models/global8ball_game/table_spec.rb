32f5d1234f93ac7de9fabe5de229cd9e0e7c128c
stephan <railsc0d0r@gmail.com>
2016-09-07 23:12:24 +0200

    Defines specs for table - for now only initializing. Refs #66, #92

--- spec/models/global8ball_game/table_spec.rb
+++ spec/models/global8ball_game/table_spec.rb
@@ -2,6 +2,47 @@ require 'rails_helper'
 
 module Global8ballGame
   RSpec.describe Table, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    before do
+      @players = create_players
+      @config = create_table_config
+      @config.deep_stringify_keys!
+    end
+
+    it "can be initialized w/ a given config" do
+      expect{Global8ballGame::Table.new @config}.to_not raise_error
+    end
+
+    private
+
+    def create_table_config
+      config = {}
+
+      config.merge!(Global8ballGame::TableConfig.new.config)
+      config.merge!(Global8ballGame::BorderConfig.new.config)
+      config.merge!(Global8ballGame::HoleConfig.new.config)
+      config.merge!(players_config)
+
+      config
+    end
+
+    def players_config
+      {
+        player_1: {
+          user_id: @players[:player_1].id,
+          name: @players[:player_1].name
+        },
+        player_2: {
+          user_id: @players[:player_2].id,
+          name: @players[:player_2].name
+        }
+      }
+    end
+
+    def create_players
+      {
+        player_1: FactoryGirl.create(:player),
+        player_2: FactoryGirl.create(:player)
+      }
+    end
   end
 end
