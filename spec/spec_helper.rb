95b4b0fdeac9d40dba23da53fcabff9b0cf8fc6f
stephan <railsc0d0r@gmail.com>
2016-09-08 19:47:17 +0200

    Moves helper-methods to spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,3 +101,34 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+def create_players
+  {
+    player_1: FactoryGirl.create(:player),
+    player_2: FactoryGirl.create(:player)
+  }
+end
+
+def create_table_config
+  config = {}
+
+  config.merge!(Global8ballGame::TableConfig.new.config)
+  config.merge!(Global8ballGame::BorderConfig.new.config)
+  config.merge!(Global8ballGame::HoleConfig.new.config)
+  config.merge!(players_config)
+
+  config
+end
+
+def players_config
+  {
+    player_1: {
+      user_id: @players[:player_1].id,
+      name: @players[:player_1].name
+    },
+    player_2: {
+      user_id: @players[:player_2].id,
+      name: @players[:player_2].name
+    }
+  }
+end
