61ba30c858ac9aa5ef2b24872aeb534a01f794b4
stephan <railsc0d0r@gmail.com>
2016-09-12 22:17:12 +0200

    Adds method to create a initial state depending on given stage to spec_helper. Refs #66, #92

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -132,3 +132,59 @@ def players_config
     }
   }
 end
+
+def initial_state player_1, player_2, stage_name="PlayForBegin", breaker=nil
+  state = {
+      current_stage: {
+          stage_name: stage_name,
+          round: 1
+        }
+    }
+
+  state.merge!(Global8ballGame::BallPositionConfig.config stage_name)
+
+  current_players = {
+    current_players: []
+  }
+
+  current_results = {
+    current_results: []
+  }
+
+  if stage_name == 'PlayForBegin'
+    state[:balls][0][:owner] = player_1
+    state[:balls][1][:owner] = player_2
+    current_players[:current_players] << { user_id: player_1}
+    current_players[:current_players] << { user_id: player_2}
+  end
+
+  if stage_name == 'PlayForVictory'
+    state[:balls][0][:owner] = breaker
+    current_players[:current_players] << { user_id: breaker}
+    current_results[:current_results] << {
+      stage_name: 'PlayForBegin',
+      winner: breaker
+    }
+  end
+
+  state.merge!(current_players)
+
+  if stage_name == 'ShowResult'
+    current_results[:current_results] << {
+      stage_name: 'PlayForBegin',
+      winner: breaker
+    }
+
+    1.upto 3 do |round|
+      current_results[:current_results] << {
+        stage_name: 'PlayForVictory',
+        round: round,
+        winner: breaker
+      }
+    end
+  end
+
+  state.merge!(current_results)
+
+  state
+end
