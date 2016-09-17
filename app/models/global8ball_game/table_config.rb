59023133bc7127eddc201fe3fe440b4b6e210b5a
stephan <railsc0d0r@gmail.com>
2016-09-17 15:35:59 +0200

    Removes cue_mass and cue_hardness from TableConfig. Adds max_breakball_speed computed from cue_mass, ball_mass and a given constant. See http://billiards.colostate.edu/threads/physics.html#properties. Refs #66, #92

--- app/models/global8ball_game/table_config.rb
+++ app/models/global8ball_game/table_config.rb
@@ -1,12 +1,16 @@
 module Global8ballGame
   class TableConfig < Config
     def initialize
+      cue_mass = 0.7
+      ball_mass = BallPositionConfig.breakball_mass
+      max_cue_speed = 5.4864 # m/s => See http://billiards.colostate.edu/threads/physics.html#properties, fast shot
+      max_breakball_speed = cue_mass / ball_mass * max_cue_speed
+
       @definition = {
         table: {
           border_bounce: 0,
           damping: 0.12 + Random.new.rand(0.05...0.1),
-          cue_hardness: 0,
-          cue_mass: 0.7, # kg
+          max_breakball_speed: max_breakball_speed,
           scaling_factor: 377.95
         }
       }
