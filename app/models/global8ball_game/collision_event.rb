b65d864c16974d055adc2d70cc2947791c05e68b
stephan <railsc0d0r@gmail.com>
2016-10-01 22:42:09 +0200

    Validates bodies in payload to be p2-bodies. Refs #66, #92, #146

--- app/models/global8ball_game/collision_event.rb
+++ app/models/global8ball_game/collision_event.rb
@@ -1,4 +1,17 @@
 module Global8ballGame
   class CollisionEvent < Event
+    def initialize payload={}
+      super
+      [:body_a, :body_b].each do |key|
+        body_name = key.to_s.split(/ |\_/).map(&:capitalize).join(" ")
+        raise "#{body_name} is not a p2-body." unless is_p2_body payload[key]
+      end
+    end
+
+    private
+
+    def is_p2_body obj
+      P2PhysicsWrapper::P2.Body.prototype.isPrototypeOf obj
+    end
   end
 end
