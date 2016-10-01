371d93fc0390e37de1e275343b21861c2effc7f8
stephan <railsc0d0r@gmail.com>
2016-10-01 11:30:06 +0200

    Implements init of rules_evaluator. Refs #66, #92, #146

--- app/models/global8ball_game/rules_evaluator.rb
+++ app/models/global8ball_game/rules_evaluator.rb
@@ -1,4 +1,13 @@
 module Global8ballGame
   class RulesEvaluator
+    attr_reader :stage
+    def initialize stage_name=nil
+      raise "No stage given to initialize RulesEvaluator" if stage_name.nil?
+      raise "Unknown stage given to initialize RulesEvaluator" unless ['PlayForBegin', 'PlayForVictory'].include? stage_name
+
+      @stage = stage_name
+    end
+
+
   end
 end
