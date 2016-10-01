67c4cba23e74193337916c45a53c80af99cbdaa0
stephan <railsc0d0r@gmail.com>
2016-10-01 22:43:37 +0200

    Adds description of basic validation of payload for shotclock_timeout_event. Refs #66, #92, #146

--- spec/models/global8ball_game/shotclock_time_out_event_spec.rb
+++ spec/models/global8ball_game/shotclock_time_out_event_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 module Global8ballGame
-  RSpec.describe TimeOutEvent, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.describe ShotclockTimeOutEvent, type: :model do
+    it "can be initialized w/ given payload" do
+      expect {ShotclockTimeOutEvent.new}.to raise_error "No payload given to be initialized"
+    end
+
+    it "checks if payload is a hash" do
+      expect {ShotclockTimeOutEvent.new "Test"}.to raise_error "Payload given has to be a hash of objects"
+    end
   end
 end
