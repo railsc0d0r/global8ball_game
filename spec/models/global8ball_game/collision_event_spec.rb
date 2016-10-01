440be13085aabba8a0cc4e6084d5c2aa1b1cd516
stephan <railsc0d0r@gmail.com>
2016-10-01 22:39:14 +0200

    Describes specs for validation of payload in collision_event. Refs #66, #92, #146

--- spec/models/global8ball_game/collision_event_spec.rb
+++ spec/models/global8ball_game/collision_event_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 module Global8ballGame
   RSpec.describe CollisionEvent, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    before do
+      @body_a = P2PhysicsWrapper::P2.Body.new
+      @body_b = P2PhysicsWrapper::P2.Body.new
+    end
+
+    it "can be initialized w/ given payload" do
+      expect {CollisionEvent.new}.to raise_error "No payload given to be initialized"
+    end
+
+    it "checks if payload is a hash" do
+      expect {CollisionEvent.new "Test"}.to raise_error "Payload given has to be a hash of objects"
+    end
+
+    it "checks if payload contains two p2-bodies" do
+      expect {CollisionEvent.new body_a: "Body A", body_b: @body_b}.to raise_error "Body A is not a p2-body."
+      expect {CollisionEvent.new body_a: @body_a, body_b: "Body B"}.to raise_error "Body B is not a p2-body."
+    end
   end
 end
