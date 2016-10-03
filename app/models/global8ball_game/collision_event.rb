74604e80f88616da6b5837a73e5cec84af47fda8
stephan <railsc0d0r@gmail.com>
2016-10-03 22:25:54 +0200

    Adds methods to fullfil the specs checking the collision_events for certain balls. Refs #66, #92, #146

--- app/models/global8ball_game/collision_event.rb
+++ app/models/global8ball_game/collision_event.rb
@@ -3,13 +3,28 @@ module Global8ballGame
     attr_reader :body_a, :body_b
     def initialize arguments={}
       super
+      not_p2_bodies = []
       [:body_a, :body_b].each do |key|
-        body_name = key.to_s.split(/ |\_/).map(&:capitalize).join(" ")
-        raise "#{body_name} is not a p2-body." unless is_p2_body arguments[key]
+        not_p2_bodies << key.to_s.split(/ |\_/).join(" ") unless is_p2_body arguments[key]
       end
+      raise "Following bodies are not p2-bodies: #{not_p2_bodies.join(', ')}." unless not_p2_bodies.empty?
 
       @body_a = arguments[:body_a]
       @body_b = arguments[:body_b]
+    end
+
+    def contains_breakball
+      (@body_a.body_type == "ball" && @body_a.ball_type == "breakball") ||
+      (@body_b.body_type == "ball" && @body_b.ball_type == "breakball")
+    end
+
+    def contains_8ball
+      (@body_a.body_type == "ball" && @body_a.ball_type == "8ball") ||
+      (@body_b.body_type == "ball" && @body_b.ball_type == "8ball")
+    end
+
+    def contains_two_balls
+      @body_a.body_type == "ball" && @body_b.body_type == "ball"
     end
 
     private
