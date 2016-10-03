c6c48abefef31a574367ce09eea509ef18fb1a46
stephan <railsc0d0r@gmail.com>
2016-10-03 12:26:20 +0200

    Renames arguments-hash. Adds attr_reader for body_a and body_b. Stores them in initializer. Refs #66, #92, #146

--- app/models/global8ball_game/collision_event.rb
+++ app/models/global8ball_game/collision_event.rb
@@ -1,11 +1,15 @@
 module Global8ballGame
   class CollisionEvent < Event
-    def initialize payload={}
+    attr_reader :body_a, :body_b
+    def initialize arguments={}
       super
       [:body_a, :body_b].each do |key|
         body_name = key.to_s.split(/ |\_/).map(&:capitalize).join(" ")
-        raise "#{body_name} is not a p2-body." unless is_p2_body payload[key]
+        raise "#{body_name} is not a p2-body." unless is_p2_body arguments[key]
       end
+
+      @body_a = arguments[:body_a]
+      @body_b = arguments[:body_b]
     end
 
     private
