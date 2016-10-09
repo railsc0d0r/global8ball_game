2753e603ea50fd54567d4281c8a2c80be19dd8e7
stephan <railsc0d0r@gmail.com>
2016-10-09 04:38:22 +0200

    Clarifies specs a little bit more. Refs #66, #92, #146

--- spec/models/global8ball_game/rule_book_spec.rb
+++ spec/models/global8ball_game/rule_book_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 module Global8ballGame
   RSpec.describe RuleBook, type: :model do
     it "returns a hash of all rules. This is empty because it's just the base class." do
-      expect(RuleBook.all_rules).to eq Hash.new
+      expect(RuleBook.all_rules).to be_empty
+      expect(RuleBook.all_rules).to be_instance_of Hash
     end
 
     it "returns an array of rules when given a event-type. This is empty because it's just the base class." do
       expect(RuleBook.rules_for :fictional_event).to be_empty
+      expect(RuleBook.rules_for :fictional_event).to be_instance_of Array
     end
   end
 end
