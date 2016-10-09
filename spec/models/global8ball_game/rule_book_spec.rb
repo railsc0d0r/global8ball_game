1ec39812848f8e8b768ad11ef18d3615aeb2296a
stephan <railsc0d0r@gmail.com>
2016-10-09 04:13:11 +0200

    Describes first specs for rule_book. Refs #66, #92, #146

--- spec/models/global8ball_game/rule_book_spec.rb
+++ spec/models/global8ball_game/rule_book_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 module Global8ballGame
   RSpec.describe RuleBook, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it "returns a hash of all rules. This is empty because it's just the base class." do
+      expect(RuleBook.all_rules).to eq Hash.new
+    end
+
+    it "returns an array of rules when given a event-type. This is empty because it's just the base class." do
+      expect(RuleBook.rules_for :fictional_event).to be_nil
+    end
   end
 end
