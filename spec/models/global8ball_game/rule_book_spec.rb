2ab287306cffbaeac65a11ec1d472a3531fd47fc
stephan <railsc0d0r@gmail.com>
2016-10-10 05:27:16 +0200

    Specifies looking for rules w/ given event and searchtag. Refs #66, #92, #146

--- spec/models/global8ball_game/rule_book_spec.rb
+++ spec/models/global8ball_game/rule_book_spec.rb
@@ -7,9 +7,13 @@ module Global8ballGame
       expect(RuleBook.all_rules).to be_instance_of Hash
     end
 
-    it "returns an array of rules when given a event-type. This is empty because it's just the base class." do
+    it "returns an array of rules when given an event-type. This is empty because it's just the base class." do
       expect(RuleBook.rules_for :fictional_event).to be_empty
       expect(RuleBook.rules_for :fictional_event).to be_instance_of Array
+    end
+
+    it "returns an array of rules when given an event-type and a search_tag." do
+      expect(RuleBook.rules_for :fictional_event, :search_tag).to be_instance_of Array
     end
   end
 end
