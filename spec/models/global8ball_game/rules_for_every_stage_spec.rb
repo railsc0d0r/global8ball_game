796ad402bbc093cde4fadfb3916cbfc4f833ff21
stephan <railsc0d0r@gmail.com>
2016-10-15 20:03:18 +0200

    Adds specs for rule_for_every_stage describing the rules for collisions and timeouts. Has to be expanded further. Refs #66, #92, #146

--- spec/models/global8ball_game/rules_for_every_stage_spec.rb
+++ spec/models/global8ball_game/rules_for_every_stage_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 module Global8ballGame
   RSpec.describe RulesForEveryStage, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    before do
+      @collision_rules = RulesForEveryStage.new.rules_for :collision_event
+      @timeout_rules = RulesForEveryStage.new.rules_for :timeout_event
+      @expected_rules = {
+        collision_event: [
+          {
+            searchtags: [:breakball_falls_into_a_hole],
+            msg: :breakball_falls_into_a_hole,
+            advice: :reinstate_breakball,
+            foul: true
+          },
+          {
+            searchtags: [:breakball_falls_into_a_hole, :ball_falls_into_a_hole],
+            msg: :ball_falls_into_a_hole,
+            advice: :remove_ball,
+            foul: false
+          }
+        ],
+        timeout_event: []
+      }
+    end
+
+    it "returns all rules for a collision_event" do
+      expect(@collision_rules).to eq @expected_rules[:collision_event]
+    end
+
+    it "returns all rules for a timeout_event" do
+      expect(@timeout_rules).to eq @expected_rules[:timeout_event]
+    end
   end
 end
