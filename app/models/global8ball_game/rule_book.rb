ec27155ab71106df2ff437d2016511b010fcbedd
stephan <railsc0d0r@gmail.com>
2016-10-15 19:51:32 +0200

    Implements RuleBook as model to be instanciated. Refs #66, #92, #146

--- app/models/global8ball_game/rule_book.rb
+++ app/models/global8ball_game/rule_book.rb
@@ -1,26 +1,30 @@
 module Global8ballGame
   class RuleBook
-    RULES = {}
-    class << self
+      def initialize
+        @rules = {
+          fictional_event: []
+        }
+      end
+
       def all_rules
-        RULES
+        @rules
       end
 
       def rules_for event=nil, search_tag=nil
         event = event.to_sym unless event.nil?
         search_tag = search_tag.to_sym unless search_tag.nil?
-        results = []
+        results = nil
 
         raise "You have to give at least an eventname to parse the rules for. Maybe you wanted to use :all_rules instead?" if event.nil?
         raise "Eventname given can't be symbolized" unless event.class == Symbol
         raise "Searchtag given can't be symbolized" if search_tag.nil? && !search_tag.class == Symbol
 
-        rules_for_event = RULES[event]
+        rules_for_event = @rules[event]
 
         if search_tag.nil?
-          results.concat rules_for_event unless rules_for_event.nil? || rules_for_event.class != Array
+          results = rules_for_event unless rules_for_event.nil? || rules_for_event.class != Array
         else
-          results.concat rules_for_tag rules_for_event unless rules_for_event.nil? || rules_for_event.class != Array
+          results = rules_for_tag rules_for_event unless rules_for_event.nil? || rules_for_event.class != Array
         end
 
         results
@@ -33,5 +37,4 @@ module Global8ballGame
         []
       end
     end
-  end
 end
