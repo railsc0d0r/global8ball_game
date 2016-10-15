88e2590f74f4e728d98a2d6eb09d6539d9c9026c
stephan <railsc0d0r@gmail.com>
2016-10-15 19:26:57 +0200

    Describes RuleBook to be used instanciated to facilitate inheritance properly. Refs #66, 92, #146

--- spec/models/global8ball_game/rule_book_spec.rb
+++ spec/models/global8ball_game/rule_book_spec.rb
@@ -3,17 +3,21 @@ require 'rails_helper'
 module Global8ballGame
   RSpec.describe RuleBook, type: :model do
     it "returns a hash of all rules. This is empty because it's just the base class." do
-      expect(RuleBook.all_rules).to be_empty
-      expect(RuleBook.all_rules).to be_instance_of Hash
+      rules = {
+            fictional_event: []
+          }
+
+      expect(RuleBook.new.all_rules).to be
+      expect(RuleBook.new.all_rules).to be_instance_of Hash
     end
 
     it "returns an array of rules when given an event-type. This is empty because it's just the base class." do
-      expect(RuleBook.rules_for :fictional_event).to be_empty
-      expect(RuleBook.rules_for :fictional_event).to be_instance_of Array
+      expect(RuleBook.new.rules_for :fictional_event).to be_empty
+      expect(RuleBook.new.rules_for :fictional_event).to be_instance_of Array
     end
 
     it "returns an array of rules when given an event-type and a search_tag." do
-      expect(RuleBook.rules_for :fictional_event, :search_tag).to be_instance_of Array
+      expect(RuleBook.new.rules_for :fictional_event, :search_tag).to be_instance_of Array
     end
   end
 end
