d68ae6d07fdb2261fd3f4074c34b968c7239a3c1
stephan <railsc0d0r@gmail.com>
2016-10-15 19:57:10 +0200

    DRY's up specs for rule_book. Refs #66, #92, #146

--- spec/models/global8ball_game/rule_book_spec.rb
+++ spec/models/global8ball_game/rule_book_spec.rb
@@ -2,22 +2,26 @@ require 'rails_helper'
 
 module Global8ballGame
   RSpec.describe RuleBook, type: :model do
-    it "returns a hash of all rules. This is empty because it's just the base class." do
-      rules = {
-            fictional_event: []
-          }
+    before do
+      @rules = {
+        fictional_event: []
+      }
+      @rule_book = RuleBook.new
+    end
 
-      expect(RuleBook.new.all_rules).to be
-      expect(RuleBook.new.all_rules).to be_instance_of Hash
+    it "returns a hash of all rules. This is empty because it's just the base class." do
+      expect(@rule_book.all_rules).to eq @rules
+      expect(@rule_book.all_rules).to be_instance_of Hash
     end
 
     it "returns an array of rules when given an event-type. This is empty because it's just the base class." do
-      expect(RuleBook.new.rules_for :fictional_event).to be_empty
-      expect(RuleBook.new.rules_for :fictional_event).to be_instance_of Array
+      expect(@rule_book.rules_for :fictional_event).to be_empty
+      expect(@rule_book.rules_for :fictional_event).to be_instance_of Array
     end
 
     it "returns an array of rules when given an event-type and a search_tag." do
-      expect(RuleBook.new.rules_for :fictional_event, :search_tag).to be_instance_of Array
+      expect(@rule_book.rules_for :fictional_event, :search_tag).to be_empty
+      expect(@rule_book.rules_for :fictional_event, :search_tag).to be_instance_of Array
     end
   end
 end
