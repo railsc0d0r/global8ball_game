3e205a9fbff68647c6ec6c37740ed68a6f08d92b
stephan <railsc0d0r@gmail.com>
2016-10-18 21:51:19 +0200

    Implements rules if breakball or another ball falls into a hole. Refs #66, #92, #146

--- app/models/global8ball_game/rules_for_every_stage.rb
+++ app/models/global8ball_game/rules_for_every_stage.rb
@@ -2,7 +2,22 @@ module Global8ballGame
   class RulesForEveryStage < RuleBook
     def initialize
       @rules = {
-        collision_event: []
+        collision_event: [
+          {
+            searchtags: [:breakball_falls_into_a_hole],
+            msg: :breakball_falls_into_a_hole,
+            advice: :reinstate_breakball,
+            foul: true,
+            conditional: false
+          },
+          {
+            searchtags: [:breakball_falls_into_a_hole, :ball_falls_into_a_hole],
+            msg: :ball_falls_into_a_hole,
+            advice: :remove_ball,
+            foul: false,
+            conditional: false
+          }
+        ]
       }
     end
   end
