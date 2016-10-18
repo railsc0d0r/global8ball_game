5d55e9d5f494ba83a6a0d01d0229e60382c4b121
stephan <railsc0d0r@gmail.com>
2016-10-18 21:48:16 +0200

    Changes spec for rule for every stage: DRY's up and defines rules for collision-event breakball falls into a hole. Refs #66, #92, #146

--- spec/models/global8ball_game/rules_for_every_stage_spec.rb
+++ spec/models/global8ball_game/rules_for_every_stage_spec.rb
@@ -3,33 +3,47 @@ require 'rails_helper'
 module Global8ballGame
   RSpec.describe RulesForEveryStage, type: :model do
     before do
-      @collision_rules = RulesForEveryStage.new.rules_for :collision_event
-      @timeout_rules = RulesForEveryStage.new.rules_for :timeout_event
+      @rules_for_every_stage = RulesForEveryStage.new
       @expected_rules = {
         collision_event: [
           {
             searchtags: [:breakball_falls_into_a_hole],
             msg: :breakball_falls_into_a_hole,
             advice: :reinstate_breakball,
-            foul: true
+            foul: true,
+            conditional: false
           },
           {
             searchtags: [:breakball_falls_into_a_hole, :ball_falls_into_a_hole],
             msg: :ball_falls_into_a_hole,
             advice: :remove_ball,
-            foul: false
+            foul: false,
+            conditional: false
           }
         ],
         timeout_event: []
       }
     end
 
-    it "returns all rules for a collision_event" do
-      expect(@collision_rules).to eq @expected_rules[:collision_event]
-    end
+    it "returns all rules given the breakball falls into a hole" do
+      event = :collision_event
+      search_tag = :breakball_falls_into_a_hole
+      expected_result = [
+        {
+          msg: :breakball_falls_into_a_hole,
+          advice: :reinstate_breakball,
+          foul: true,
+          conditional: false
+        },
+        {
+          msg: :ball_falls_into_a_hole,
+          advice: :remove_ball,
+          foul: false,
+          conditional: false
+        }
+      ]
 
-    it "returns all rules for a timeout_event" do
-      expect(@timeout_rules).to eq @expected_rules[:timeout_event]
+      expect(@rules_for_every_stage.rules_for event, search_tag).to eq expected_result
     end
   end
 end
