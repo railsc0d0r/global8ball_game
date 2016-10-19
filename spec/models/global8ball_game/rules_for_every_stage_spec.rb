2959f227ec6781ccfb8201074252de6b72c94223
stephan <railsc0d0r@gmail.com>
2016-10-19 04:09:45 +0200

    Adds another spec for rules concerning the eightball falling into a hole. Refs #66, #92, #146

--- spec/models/global8ball_game/rules_for_every_stage_spec.rb
+++ spec/models/global8ball_game/rules_for_every_stage_spec.rb
@@ -27,6 +27,27 @@ module Global8ballGame
       expect(@rules_for_every_stage.rules_for event, search_tag).to eq expected_result
     end
 
+    it "returns all rules given the eightball falls into a hole" do
+      event = :collision_event
+      search_tag = :eightball_falls_into_a_hole
+      expected_result = [
+        {
+          msg: :eightball_falls_into_a_hole,
+          advice: :round_lost,
+          foul: true,
+          conditional: true
+        },
+        {
+          msg: :ball_falls_into_a_hole,
+          advice: :remove_ball,
+          foul: false,
+          conditional: false
+        }
+      ]
+
+      expect(@rules_for_every_stage.rules_for event, search_tag).to eq expected_result
+    end
+
     it "returns all rules given a ball falls into a hole" do
       event = :collision_event
       search_tag = :ball_falls_into_a_hole
