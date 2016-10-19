6c88f242f303517db33d1f6b8485b60e680a78dc
stephan <railsc0d0r@gmail.com>
2016-10-19 04:38:54 +0200

    Implements rules concerning the eightball falling into a hole as depending on certain conditions. Refs #66, #92, #146

--- app/models/global8ball_game/rules_for_play_for_victory.rb
+++ app/models/global8ball_game/rules_for_play_for_victory.rb
@@ -5,10 +5,19 @@ module Global8ballGame
         collision_event: [
           {
             searchtags: [:eightball_falls_into_a_hole],
-            msg: :eightball_falls_into_a_hole,
+            msg: :round_lost,
             advice: :round_lost,
             foul: true,
-            conditional: true
+            conditional: true,
+            condition: :breaker_is_not_eightball_owner
+          },
+          {
+            searchtags: [:eightball_falls_into_a_hole],
+            msg: :round_won,
+            advice: :round_won,
+            foul: false,
+            conditional: true,
+            condition: :breaker_is_eightball_owner
           }
         ]
       }
