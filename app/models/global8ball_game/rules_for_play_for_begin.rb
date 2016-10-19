6b1c4e56aef23c391014b2fa55ff52750aae9f81
stephan <railsc0d0r@gmail.com>
2016-10-19 04:22:04 +0200

    Implements another spec for rules concerning the breakball crossing the center_line in PlayForBegin. Refs #66, #92, #146

--- app/models/global8ball_game/rules_for_play_for_begin.rb
+++ app/models/global8ball_game/rules_for_play_for_begin.rb
@@ -1,4 +1,18 @@
 module Global8ballGame
   class RulesForPlayForBegin < RuleBook
+    def initialize
+      @rules = {
+        collision_event: [
+          {
+            searchtags: [:breakball_crosses_center_line],
+            msg: :breakball_crosses_center_line,
+            advice: :restart_round,
+            foul: true,
+            conditional: false
+          }
+        ],
+        timeout_event: []
+      }
+    end
   end
 end
