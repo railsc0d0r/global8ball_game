c673ac7938b2dba11efc981118bbf857f25b6269
stephan <railsc0d0r@gmail.com>
2016-10-22 22:15:36 +0200

    Adds a concern to outsource body-creation into special models. Refs #66, #92

---
+++ app/models/concerns/global8ball_game/body_creation_concern.rb
@@ -0,0 +1,49 @@
+#
+# Global8ballGame::PhysicsConcern
+#
+# Lets model including this concern instanciate a table by given config and last_result
+# and evaluate a shot on this table
+#
+# Models including this concern have to provide config and last_result as hashes representing the table_config and the last state
+#
+module Global8ballGame
+  module BodyCreationConcern
+    extend ActiveSupport::Concern
+
+    included do
+      attr_reader :body
+
+      BORDER = 2 ** 2
+      HOLE = 2 ** 3
+      BALL = 2 ** 4
+      LINE = 2 ** 5
+
+      BORDER_COLLIDES_WITH = BALL
+      HOLE_COLLIDES_WITH = BALL
+      LINE_COLLIDES_WITH = BALL
+      BALL_COLLIDES_WITH = BORDER | HOLE | BALL | LINE
+    end
+
+    protected
+
+    def create_body body_type, key, options, shape
+      body = P2PhysicsWrapper::P2.Body.new options
+      body.body_type = body_type
+      body.key = key
+      body.addShape shape
+      body
+    end
+
+    def convex vertices
+      P2PhysicsWrapper::P2.Convex.new({vertices: vertices})
+    end
+
+    def circle radius
+      P2PhysicsWrapper::P2.Circle.new({ radius: radius })
+    end
+
+    def line length
+      P2PhysicsWrapper::P2.Line.new({ length: length })
+    end
+  end
+end
