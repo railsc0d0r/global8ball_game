2f131d989e000b44e63672a934f6f7e5f7b92684
stephan <railsc0d0r@gmail.com>
2016-10-23 04:38:01 +0200

    Specifies and implements border as model using BodyCreationConcern to unclutter table.rb. Refs #66, #92

---
+++ app/models/global8ball_game/border.rb
@@ -0,0 +1,24 @@
+module Global8ballGame
+  class Border
+    include BodyCreationConcern
+
+    def initialize key, vertices, material
+      body_type = "border"
+
+      body_options = {
+        mass: 0,
+        position: [0, 0],
+        angle: 0,
+        velocity: [0, 0],
+        angularVelocity: 0
+      }
+
+      shape = convex vertices
+      shape.collisionGroup = BORDER
+      shape.collisionMask = BORDER_COLLIDES_WITH
+      shape.material = material
+
+      @body = create_body body_type, key, body_options, shape
+    end
+  end
+end
