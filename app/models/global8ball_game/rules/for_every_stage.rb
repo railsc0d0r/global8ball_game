107df2277bb53f5cf8c6c74e0f7eec249b2cc94b
stephan <railsc0d0r@gmail.com>
2016-10-23 23:33:36 +0200

    Moves RulesForEveryStage to Rules::ForEveryStage. Refs #66, #92, #146

---
+++ app/models/global8ball_game/rules/for_every_stage.rb
@@ -0,0 +1,27 @@
+module Global8ballGame
+  module Rules
+    class ForEveryStage < Book
+      def initialize
+        @rules = {
+          collision: [
+            {
+              searchtags: [:breakball_falls_into_a_hole],
+              msg: :breakball_falls_into_a_hole,
+              advice: :reinstate_breakball,
+              foul: true,
+              conditional: false
+            },
+            {
+              searchtags: [:breakball_falls_into_a_hole, :eightball_falls_into_a_hole, :ball_falls_into_a_hole],
+              msg: :ball_falls_into_a_hole,
+              advice: :remove_ball,
+              foul: false,
+              conditional: false
+            }
+          ],
+          timeout_event: []
+        }
+      end
+    end
+  end
+end
