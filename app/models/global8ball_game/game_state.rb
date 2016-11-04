13ecf84670b1e1f22606b1846cf2b1090064556b
stephan <railsc0d0r@gmail.com>
2016-11-04 05:04:51 +0100

    Implements GameState as specified: returns attributes as hash and stores balls, too. Refs #66, #92, #146

--- app/models/global8ball_game/game_state.rb
+++ app/models/global8ball_game/game_state.rb
@@ -6,8 +6,22 @@ module Global8ballGame
     def initialize state
       @stage_name = state['current_stage']['stage_name']
       @round = state['current_stage']['round']
+      @balls = state['balls']
       @current_players = state['current_players']
       @current_results = state['current_results']
+    end
+
+    def to_hash
+      hash = {
+        current_stage: {
+          stage_name: @stage_name,
+          round: @round
+        },
+        balls: @balls,
+        current_players: @current_players,
+        current_results: @current_results
+      }
+      hash.deep_stringify_keys
     end
   end
 end
