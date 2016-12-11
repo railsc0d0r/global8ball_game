3199f3367650d14c685c9f6f9e1544a53851eeff
stephan <railsc0d0r@gmail.com>
2016-12-11 08:32:38 +0100

    Refactors ShotResult as specified. Refs #66, #92, #146

--- app/models/global8ball_game/shot_result.rb
+++ app/models/global8ball_game/shot_result.rb
@@ -2,20 +2,32 @@ module Global8ballGame
   class ShotResult
     attr_reader :shot, :events
     attr_accessor :foul
-    def initialize shot
-      raise "Shot given isn't a valid Shot-object." unless shot.class == Shot
+    def initialize shot = nil
+      validate_shot shot
+
       @shot = shot
       @foul = false
       @events = Heap.new
     end
 
+    def shot= shot
+      validate_shot shot
+      @shot = shot
+    end
+
     def to_hash
       result = {
-        shot: @shot.to_hash,
+        shot: @shot.nil? ? {} : @shot.to_hash,
         foul: @foul,
         events: @events.to_a.uniq
       }
       result.deep_stringify_keys
+    end
+
+    private
+
+    def validate_shot shot
+      raise "Shot given isn't a valid Shot-object." unless shot.nil? || shot.class == Shot
     end
   end
 end
