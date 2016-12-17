ea68d26b988ca46428ec6d283d54292208c2738e
stephan <railsc0d0r@gmail.com>
2016-12-17 22:33:42 +0100

    Moves Hole into namespace Physics. Refs #66, #92

---
+++ app/models/global8ball_game/physics/hole.rb
@@ -0,0 +1,24 @@
+module Global8ballGame
+  module Physics
+    class Hole
+      include BodyCreationConcern
+
+      def initialize key, radius, position
+        body_type = "hole"
+        body_options = {
+          mass:0,
+          position: position,
+          angle: 0,
+          velocity: [0, 0],
+          angularVelocity: 0
+        }
+
+        shape = circle radius
+        shape.collisionGroup = HOLE
+        shape.collisionMask = HOLE_COLLIDES_WITH
+
+        @body = create_body body_type, key, body_options, shape
+      end
+    end
+  end
+end
