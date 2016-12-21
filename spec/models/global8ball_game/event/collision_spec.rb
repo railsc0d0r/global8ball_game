3db9d5ae45d01fb53317a111e7f5600d7546f6f6
stephan <railsc0d0r@gmail.com>
2016-12-21 02:01:23 +0100

    Fixes spec to used the hash returned by ObjectCreator. Refs #66, #92, #146

--- spec/models/global8ball_game/event/collision_spec.rb
+++ spec/models/global8ball_game/event/collision_spec.rb
@@ -5,7 +5,7 @@ module Global8ballGame
     RSpec.describe Collision, type: :model do
       before do
         @object_creator = ObjectCreator.new
-        @breakball, @playball, @playball2, @eightball, @center_line, @right_border, @right_top_hole, @left_border, @left_top_hole = @object_creator.create_bodies_for_collision_events
+        @bodies = @object_creator.create_bodies_for_collision_events
       end
 
       it "can be initialized w/ given payload" do
@@ -17,146 +17,222 @@ module Global8ballGame
       end
 
       it "checks if payload contains two p2-bodies" do
-        expect {Collision.new body_a: "Body A", body_b: @playball}.to raise_error "Following bodies are not p2-bodies: body a."
-        expect {Collision.new body_a: @breakball, body_b: "Body B"}.to raise_error "Following bodies are not p2-bodies: body b."
+        expect {Collision.new body_a: "Body A", body_b: @bodies[:balls][:playball]}.to raise_error "Following bodies are not p2-bodies: body a."
+        expect {Collision.new body_a: @bodies[:balls][:breakball], body_b: "Body B"}.to raise_error "Following bodies are not p2-bodies: body b."
         expect {Collision.new body_a: "Body A", body_b: "Body B"}.to raise_error "Following bodies are not p2-bodies: body a, body b."
       end
 
       it "stores given bodies in instance-vars and provides setters to access them" do
-        ce = Collision.new body_a: @breakball, body_b: @playball
-        expect(ce.body_a).to be @breakball
-        expect(ce.body_b).to be @playball
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+
+        ce = Collision.new body_a: breakball, body_b: playball
+        expect(ce.body_a).to be breakball
+        expect(ce.body_b).to be playball
       end
 
       it "checks if the event contains a breakball" do
-        ce = Collision.new body_a: @breakball, body_b: @playball
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+        eightball = @bodies[:balls][:eightball]
+
+        ce = Collision.new body_a: breakball, body_b: playball
         expect(ce.contains_breakball).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @breakball
+        ce = Collision.new body_a: breakball, body_b: breakball
         expect(ce.contains_breakball).to be_truthy
-        ce = Collision.new body_a: @playball, body_b: @eightball
+        ce = Collision.new body_a: playball, body_b: eightball
         expect(ce.contains_breakball).to be_falsy
       end
 
       it "checks if the event contains the 8ball" do
-        ce = Collision.new body_a: @breakball, body_b: @eightball
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+        eightball = @bodies[:balls][:eightball]
+
+        ce = Collision.new body_a: breakball, body_b: eightball
         expect(ce.contains_8ball).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @playball
+        ce = Collision.new body_a: breakball, body_b: playball
         expect(ce.contains_8ball).to be_falsy
       end
 
       it "checks if the event contains two balls" do
-        ce = Collision.new body_a: @breakball, body_b: @playball
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+        playball_2 = @bodies[:balls][:playball_2]
+        center_line = @bodies[:center_line]
+
+        ce = Collision.new body_a: breakball, body_b: playball
         expect(ce.contains_two_balls).to be_truthy
-        ce = Collision.new body_a: @playball, body_b: @playball2
+        ce = Collision.new body_a: playball, body_b: playball_2
         expect(ce.contains_two_balls).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @center_line
+        ce = Collision.new body_a: breakball, body_b: center_line
         expect(ce.contains_two_balls).to be_falsy
       end
 
       it "checks if the event contains only one ball" do
-        ce = Collision.new body_a: @breakball, body_b: @center_line
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+        center_line = @bodies[:center_line]
+
+        ce = Collision.new body_a: breakball, body_b: center_line
         expect(ce.contains_one_ball).to be_truthy
 
-        ce = Collision.new body_a: @breakball, body_b: @playball
+        ce = Collision.new body_a: breakball, body_b: playball
         expect(ce.contains_one_ball).to be_falsy
       end
 
       it "checks if the event contains the center line" do
-        ce = Collision.new body_a: @breakball, body_b: @center_line
+        breakball = @bodies[:balls][:breakball]
+        center_line = @bodies[:center_line]
+        right_border = @bodies[:borders][:right_border]
+
+        ce = Collision.new body_a: breakball, body_b: center_line
         expect(ce.contains_center_line).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @right_border
+        ce = Collision.new body_a: breakball, body_b: right_border
         expect(ce.contains_center_line).to be_falsy
       end
 
       it "checks if the event contains the right border" do
-        ce = Collision.new body_a: @breakball, body_b: @right_border
+        breakball = @bodies[:balls][:breakball]
+        right_border = @bodies[:borders][:right_border]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+
+        ce = Collision.new body_a: breakball, body_b: right_border
         expect(ce.contains_right_border).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @right_top_hole
+        ce = Collision.new body_a: breakball, body_b: right_top_hole
         expect(ce.contains_right_border).to be_falsy
       end
 
       it "checks if the event contains the left border" do
-        ce = Collision.new body_a: @breakball, body_b: @left_border
+        breakball = @bodies[:balls][:breakball]
+        left_border = @bodies[:borders][:left_border]
+        left_top_hole = @bodies[:holes][:left_top_hole]
+
+        ce = Collision.new body_a: breakball, body_b: left_border
         expect(ce.contains_left_border).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @left_top_hole
+        ce = Collision.new body_a: breakball, body_b: left_top_hole
         expect(ce.contains_left_border).to be_falsy
       end
 
       it "checks if the event contains a hole" do
-        ce = Collision.new body_a: @breakball, body_b: @right_top_hole
+        breakball = @bodies[:balls][:breakball]
+        right_border = @bodies[:borders][:right_border]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+
+        ce = Collision.new body_a: breakball, body_b: right_top_hole
         expect(ce.contains_hole).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @right_border
+        ce = Collision.new body_a: breakball, body_b: right_border
         expect(ce.contains_hole).to be_falsy
       end
 
       it "checks if a ball falls into a hole" do
-        ce = Collision.new body_a: @breakball, body_b: @right_top_hole
+        breakball = @bodies[:balls][:breakball]
+        right_border = @bodies[:borders][:right_border]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+
+        ce = Collision.new body_a: breakball, body_b: right_top_hole
         expect(ce.ball_falls_into_a_hole).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @right_border
+        ce = Collision.new body_a: breakball, body_b: right_border
         expect(ce.ball_falls_into_a_hole).to be_falsy
       end
 
       it "checks if the breakball falls into a hole" do
-        ce = Collision.new body_a: @breakball, body_b: @right_top_hole
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+        right_border = @bodies[:borders][:right_border]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+
+        ce = Collision.new body_a: breakball, body_b: right_top_hole
         expect(ce.breakball_falls_into_a_hole).to be_truthy
-        ce = Collision.new body_a: @breakball, body_b: @right_border
+        ce = Collision.new body_a: breakball, body_b: right_border
         expect(ce.breakball_falls_into_a_hole).to be_falsy
-        ce = Collision.new body_a: @playball, body_b: @right_top_hole
+        ce = Collision.new body_a: playball, body_b: right_top_hole
         expect(ce.breakball_falls_into_a_hole).to be_falsy
       end
 
       it "checks if the 8ball falls into a hole" do
-        ce = Collision.new body_a: @eightball, body_b: @right_top_hole
+        eightball = @bodies[:balls][:eightball]
+        playball = @bodies[:balls][:playball]
+        right_border = @bodies[:borders][:right_border]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+
+        ce = Collision.new body_a: eightball, body_b: right_top_hole
         expect(ce.eightball_falls_into_a_hole).to be_truthy
-        ce = Collision.new body_a: @eightball, body_b: @right_border
+        ce = Collision.new body_a: eightball, body_b: right_border
         expect(ce.eightball_falls_into_a_hole).to be_falsy
-        ce = Collision.new body_a: @playball, body_b: @right_top_hole
+        ce = Collision.new body_a: playball, body_b: right_top_hole
         expect(ce.eightball_falls_into_a_hole).to be_falsy
       end
 
       it "checks if the breakball crosses the center-line" do
-        ce = Collision.new body_a: @breakball, body_b: @center_line
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+        right_border = @bodies[:borders][:right_border]
+        center_line = @bodies[:center_line]
+
+        ce = Collision.new body_a: breakball, body_b: center_line
         expect(ce.breakball_crosses_center_line).to be_truthy
-        ce = Collision.new body_a: @playball, body_b: @center_line
+        ce = Collision.new body_a: playball, body_b: center_line
         expect(ce.breakball_crosses_center_line).to be_falsy
-        ce = Collision.new body_a: @breakball, body_b: @right_border
+        ce = Collision.new body_a: breakball, body_b: right_border
         expect(ce.breakball_crosses_center_line).to be_falsy
       end
 
       it "checks if breakball collides w/ eightball" do
-        ce = Collision.new body_a: @breakball, body_b: @eightball
+        breakball = @bodies[:balls][:breakball]
+        eightball = @bodies[:balls][:eightball]
+
+        ce = Collision.new body_a: breakball, body_b: eightball
         expect(ce.breakball_collides_with_eightball).to be_truthy
       end
 
       it "returns a symbol describing the evaluated event to be used as searchtag in rules-parsing" do
-        ce = Collision.new body_a: @playball, body_b: @right_top_hole
+        breakball = @bodies[:balls][:breakball]
+        eightball = @bodies[:balls][:eightball]
+        playball = @bodies[:balls][:playball]
+        center_line = @bodies[:center_line]
+        right_border = @bodies[:borders][:right_border]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+        left_border = @bodies[:borders][:left_border]
+
+        ce = Collision.new body_a: playball, body_b: right_top_hole
         expect(ce.kind_of_event).to eq :ball_falls_into_a_hole
-        ce = Collision.new body_a: @breakball, body_b: @right_top_hole
+        ce = Collision.new body_a: breakball, body_b: right_top_hole
         expect(ce.kind_of_event).to eq :breakball_falls_into_a_hole
-        ce = Collision.new body_a: @eightball, body_b: @right_top_hole
+        ce = Collision.new body_a: eightball, body_b: right_top_hole
         expect(ce.kind_of_event).to eq :eightball_falls_into_a_hole
-        ce = Collision.new body_a: @breakball, body_b: @center_line
+        ce = Collision.new body_a: breakball, body_b: center_line
         expect(ce.kind_of_event).to eq :breakball_crosses_center_line
-        ce = Collision.new body_a: @breakball, body_b: @eightball
+        ce = Collision.new body_a: breakball, body_b: eightball
         expect(ce.kind_of_event).to eq :breakball_collides_with_eightball
-        ce = Collision.new body_a: @breakball, body_b: @right_border
+        ce = Collision.new body_a: breakball, body_b: right_border
         expect(ce.kind_of_event).to eq :standard_collision
       end
 
       it "raises an error, if we try to get a ball but event contains two." do
-        ce = Collision.new body_a: @playball, body_b: @breakball
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+
+        ce = Collision.new body_a: playball, body_b: breakball
         expect{ce.get_ball}.to raise_error "Don't know which ball to return."
       end
 
       it "returns the ball, if the event contains only one" do
-        ce = Collision.new body_a: @breakball, body_b: @right_top_hole
-        expect(ce.get_ball).to be @breakball
-        ce = Collision.new body_a: @right_top_hole, body_b: @playball
-        expect(ce.get_ball).to be @playball
+        breakball = @bodies[:balls][:breakball]
+        playball = @bodies[:balls][:playball]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+
+        ce = Collision.new body_a: breakball, body_b: right_top_hole
+        expect(ce.get_ball).to be breakball
+        ce = Collision.new body_a: right_top_hole, body_b: playball
+        expect(ce.get_ball).to be playball
       end
 
       it "returns nil, if the event doesn't contain a ball" do
-        ce = Collision.new body_a: @center_line, body_b: @right_top_hole
+        center_line = @bodies[:center_line]
+        right_top_hole = @bodies[:holes][:right_top_hole]
+
+        ce = Collision.new body_a: center_line, body_b: right_top_hole
         expect(ce.get_ball).to be nil
       end
     end
