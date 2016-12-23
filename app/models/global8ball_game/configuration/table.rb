89c91a5df38ba52de33661eda0db2c6ba797e1f5
stephan <railsc0d0r@gmail.com>
2016-12-23 20:42:20 +0100

    Implements minimum_ball_speed in TableConfig as specified. Refs #66, #92, #157

--- app/models/global8ball_game/configuration/table.rb
+++ app/models/global8ball_game/configuration/table.rb
@@ -6,11 +6,13 @@ module Global8ballGame
         ball_mass = BallPosition.breakball_mass
         max_cue_speed = 2.7432 # m/s => See http://billiards.colostate.edu/threads/physics.html#properties, fast shot
         max_breakball_speed = cue_mass / ball_mass * max_cue_speed
+        min_ball_speed = 0.000001
 
         @definition = {
           table: {
             damping: 0.12 + Random.new.rand(0.05...0.1),
             max_breakball_speed: max_breakball_speed,
+            min_ball_speed: min_ball_speed,
             scaling_factor: 377.95,
             contact_materials: {
               ball_border: {
