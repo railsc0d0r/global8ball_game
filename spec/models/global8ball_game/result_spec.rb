778dea488db0db25732b0331f6160f58c95c7b5a
stephan <railsc0d0r@gmail.com>
2016-12-26 23:51:37 +0100

    Refactors spec for result to query Redis instead of using already loaded instance. Refs #66, #159

--- spec/models/global8ball_game/result_spec.rb
+++ spec/models/global8ball_game/result_spec.rb
@@ -4,9 +4,9 @@ module Global8ballGame
   RSpec.describe Result, type: :model do
     it "provides an attribute called content to store json as string" do
       content = "123"
-      result = Result.create content: "123"
+      Result.create content: "123"
 
-      expect(result.content).to eq content
+      expect(Result.all.first.content).to eq content
     end
 
     it "provides a method to convert given hash to JSON and store it as string in content" do
@@ -18,7 +18,7 @@ module Global8ballGame
       result = Result.create
       result.result_set = content
 
-      expect(result.content).to eq content.to_json
+      expect(Result.all.first.content).to eq content.to_json
     end
   end
 end
