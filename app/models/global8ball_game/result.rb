7be0924d8bdc49f81fa7dca1f27c401432652045
stephan <railsc0d0r@gmail.com>
2016-12-27 00:01:45 +0100

    Implements getter for result_set as wrapper as specified. Refs #66, #159

--- app/models/global8ball_game/result.rb
+++ app/models/global8ball_game/result.rb
@@ -2,6 +2,10 @@ module Global8ballGame
   class Result < Ohm::Model
     attribute :content
 
+    def result_set
+      JSON.parse content
+    end
+
     def result_set= content_hash
       self.content = content_hash.to_json
       self.save
