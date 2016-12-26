92e478f5d244919c28518c6c92230f3f00ef18ce
stephan <railsc0d0r@gmail.com>
2016-12-26 23:45:40 +0100

    Adds expectation for 1st spec. Adds spec for wrapper to convert a hash into a json-string and store it as content. Refs #66, #159

--- spec/models/global8ball_game/result_spec.rb
+++ spec/models/global8ball_game/result_spec.rb
@@ -3,7 +3,22 @@ require 'rails_helper'
 module Global8ballGame
   RSpec.describe Result, type: :model do
     it "provides an attribute called content to store json as string" do
+      content = "123"
       result = Result.create content: "123"
+
+      expect(result.content).to eq content
+    end
+
+    it "provides a method to convert given hash to JSON and store it as string in content" do
+      content = {
+        a: 1,
+        b: "123"
+      }
+
+      result = Result.create
+      result.result_set = content
+
+      expect(result.content).to eq content.to_json
     end
   end
 end
