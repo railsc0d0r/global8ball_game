526d012d982f310899f5f0f9e894bfb6773ef553
stephan <railsc0d0r@gmail.com>
2016-12-27 22:59:42 +0100

    Refactors spec to DRY it up. Specifies result to belong to a Game. Refs #66, #159

--- spec/models/global8ball_game/result_spec.rb
+++ spec/models/global8ball_game/result_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 module Global8ballGame
   RSpec.describe Result, type: :model do
+    before do
+      @content = {
+        a: 1,
+        b: "123"
+      }
+    end
+
     it "provides an attribute called content to store json as string" do
       content = "123"
       Result.create content: "123"
@@ -10,26 +17,21 @@ module Global8ballGame
     end
 
     it "provides a method to convert given hash to JSON and store it as string in content" do
-      content = {
-        a: 1,
-        b: "123"
-      }
-
-      result = Result.new
-      result.result_set = content
-      result.save
-
-      expect(Result.all.first.content).to eq content.to_json
+      Result.create result_set: @content
+      expect(Result.all.first.content).to eq @content.to_json
     end
 
     it "provides a method to convert stored content-string to hash and return it" do
-      content = {
-        a: 1,
-        b: "123"
-      }
+      Result.create content: @content.to_json
+      expect(Result.all.first.result_set).to eq @content.deep_stringify_keys
+    end
 
-      Result.create content: content.to_json
-      expect(Result.all.first.result_set).to eq content.deep_stringify_keys
+    it "belongs to a Game" do
+      object_creator = ObjectCreator.new
+      game = Game.create!
+
+      Result.create result_set: @content, game: game
+      expect(Result.all.first.game).to eq game
     end
   end
 end
