90cff6b6adc21a504c71c038b6ff9e1c0c251b2b
stephan <railsc0d0r@gmail.com>
2016-12-27 23:01:30 +0100

    Implements reference to game in result as specified. Refs #66, #159

--- app/models/global8ball_game/result.rb
+++ app/models/global8ball_game/result.rb
@@ -1,6 +1,7 @@
 module Global8ballGame
   class Result < Ohm::Model
     attribute :content
+    reference :game, :Game
 
     def result_set
       JSON.parse content
@@ -8,6 +9,10 @@ module Global8ballGame
 
     def result_set= content_hash
       self.content = content_hash.to_json
+    end
+
+    def game
+      Game.find game_id
     end
   end
 end
