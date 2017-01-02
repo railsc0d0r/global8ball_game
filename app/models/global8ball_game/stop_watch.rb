cb6947937b0c8d69918b1748141637ee674e391a
stephan <railsc0d0r@gmail.com>
2017-01-03 00:38:25 +0100

    Scaffolds StopWatch as Ohm-model. Refs #66, #87, #158

---
+++ app/models/global8ball_game/stop_watch.rb
@@ -0,0 +1,4 @@
+module Global8ballGame
+  class StopWatch < Ohm::Model
+  end
+end
---
+++ spec/models/global8ball_game/stop_watch_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module Global8ballGame
+  RSpec.describe StopWatch, type: :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
