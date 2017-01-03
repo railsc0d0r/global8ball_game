7b0fe5232da8a3ff81c2d2cd0f492bd7bad5e444
stephan <railsc0d0r@gmail.com>
2017-01-03 01:00:07 +0100

    Specifies StopWatch to store timestamps for start and stop. Refs #66, #87, #158

--- spec/models/global8ball_game/stop_watch_spec.rb
+++ spec/models/global8ball_game/stop_watch_spec.rb
@@ -12,5 +12,18 @@ module Global8ballGame
       expect(StopWatch.all.first.game).to eq @game
     end
 
+    it "stores a start-timestamp" do
+      start = Time.at(Time.now.to_i)
+      StopWatch.create game: @game, start: start
+      expect(StopWatch.all.first.start).to be_kind_of Time
+      expect(StopWatch.all.first.start).to eq start
+    end
+
+    it "stores a stop-timestamp" do
+      stop = Time.at(Time.now.since(10).to_i)
+      StopWatch.create game: @game, stop: stop
+      expect(StopWatch.all.first.stop).to be_kind_of Time
+      expect(StopWatch.all.first.stop).to eq stop
+    end
   end
 end
