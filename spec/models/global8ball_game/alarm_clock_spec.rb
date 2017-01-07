3ac91f32bbdeb3f96eea6662097f07d6dd453733
stephan <railsc0d0r@gmail.com>
2017-01-07 18:40:09 +0100

    Specifies AlarmClock to validate presence of player. Refs #66, #87, #158

--- spec/models/global8ball_game/alarm_clock_spec.rb
+++ spec/models/global8ball_game/alarm_clock_spec.rb
@@ -8,19 +8,20 @@ module Global8ballGame
       @time_now = Time.now
       @finish = Time.at(@time_now.in(5.seconds).to_i)
       @context = :shotclock
+      @player = @game.player_1
     end
 
     it "belongs to a Game" do
-      AlarmClock.create game: @game, finish: @finish, context: @context
+      AlarmClock.create game: @game, finish: @finish, context: @context, player: @player
       expect(AlarmClock.all.first.game).to eq @game
     end
 
     it "validates presence of Game" do
-      expect {AlarmClock.create finish: @finish, context: @context}.to raise_error "No game given to create AlarmClock."
+      expect {AlarmClock.create finish: @finish, context: @context, player: @player}.to raise_error "No game given to create AlarmClock."
     end
 
     it "stores a finish-timestamp" do
-      AlarmClock.create game: @game, finish: @finish, context: @context
+      AlarmClock.create game: @game, finish: @finish, context: @context, player: @player
       result_clock = AlarmClock.all.first
 
       expect(result_clock.finish).to be_kind_of Time
@@ -28,11 +29,11 @@ module Global8ballGame
     end
 
     it "validates presence of :finish" do
-      expect {AlarmClock.create game: @game, context: @context}.to raise_error "No :finish given as timestamp to define when clock runs out."
+      expect {AlarmClock.create game: @game, context: @context, player: @player}.to raise_error "No :finish given as timestamp to define when clock runs out."
     end
 
     it "stores its context" do
-      AlarmClock.create game: @game, finish: @finish, context: @context
+      AlarmClock.create game: @game, finish: @finish, context: @context, player: @player
       result_clock = AlarmClock.all.first
 
       expect(result_clock.context).to be_kind_of Symbol
@@ -40,25 +41,28 @@ module Global8ballGame
     end
 
     it "validates presence of :context" do
-      expect {AlarmClock.create game: @game, finish: @finish}.to raise_error "No :context given for AlarmClock."
+      expect {AlarmClock.create game: @game, finish: @finish, player: @player}.to raise_error "No :context given for AlarmClock."
     end
 
     it "stores the player this alarm_clock belongs to" do
-      player = @game.player_1
-      alarm_clock = AlarmClock.create game: @game, finish: @finish, player: player, context: @context
+      AlarmClock.create game: @game, finish: @finish, player: @player, context: @context
 
-      expect(AlarmClock.all.first.player).to eq player
+      expect(AlarmClock.all.first.player).to eq @player
+    end
+
+    it "validates presence of :player" do
+      expect {AlarmClock.create game: @game, finish: @finish, context: @context}.to raise_error "No player given for AlarmClock."
     end
 
     it "provides timestamps on create and update" do
-      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context
+      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context, player: @player
 
       expect(alarm_clock.created_at).to be_kind_of Time
       expect(alarm_clock.updated_at).to be_kind_of Time
     end
 
     it "can tell if the alarm-time passed already" do
-      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context
+      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context, player: @player
 
       Timecop.freeze(Time.at(@time_now.in(4.seconds).to_i)) do
         expect(alarm_clock.finished?).to be_falsy
@@ -70,7 +74,7 @@ module Global8ballGame
     end
 
     it "can check the alarm_clock and publishes an event if the alarm is to be sounded" do
-      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context
+      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context, player: @player
       result = false
 
       alarm_clock.on(:sound_the_alarm) do
@@ -89,7 +93,7 @@ module Global8ballGame
     end
 
     it "can add x seconds to finish-timestamp and persist the new timestamp" do
-      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context
+      alarm_clock = AlarmClock.create game: @game, finish: @finish, context: @context, player: @player
       alarm_clock.add_seconds 20
       expected_result = @finish.in(20.seconds)
 
