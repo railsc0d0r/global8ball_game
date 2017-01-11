b1dee5901431c3433a525a7d28072c0a6fdc2ad5
stephan <railsc0d0r@gmail.com>
2017-01-11 04:03:26 +0100

    Refactors specs for Result to properly describe :result_set as Type::Hash and new validation-implementation w/ ModelValidationConcern. Refs #66, #92, #162

--- spec/models/global8ball_game/result_spec.rb
+++ spec/models/global8ball_game/result_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 module Global8ballGame
   RSpec.describe Result, type: :model do
     before do
-      @content = {
+      @result_set = {
         a: 1,
         b: "123"
       }
@@ -12,38 +12,26 @@ module Global8ballGame
       @game = Game.create!
     end
 
-    it "provides an attribute called content to store json as string" do
-      content = "123"
-      Result.create content: "123", game: @game
-
-      expect(Result.all.first.content).to eq content
-    end
-
-    it "provides a method to convert given hash to JSON and store it as string in content" do
-      Result.create result_set: @content, game: @game
-      expect(Result.all.first.content).to eq @content.to_json
-    end
-
-    it "provides a method to convert stored content-string to hash and return it" do
-      Result.create content: @content.to_json, game: @game
-      expect(Result.all.first.result_set).to eq @content.deep_stringify_keys
+    it "provides an attribute to store a result_set-hash" do
+      Result.create result_set: @result_set, game: @game
+      expect(Result.all.first.result_set).to eq @result_set.deep_stringify_keys
     end
 
     it "belongs to a Game" do
-      Result.create result_set: @content, game: @game
+      Result.create result_set: @result_set, game: @game
       expect(Result.all.first.game).to eq @game
     end
 
-    it "validates presence of content" do
-      expect {Result.create game: @game}.to raise_error "No content/result_set given for Result."
+    it "validates presence of :result_set" do
+      expect {Result.create game: @game}.to raise_error "Global8ballGame::Result is not valid. Errors: result_set is not present."
     end
 
     it "validates presence of a game" do
-      expect {Result.create result_set: @content}.to raise_error "No game given for Result."
+      expect {Result.create result_set: @result_set}.to raise_error "Global8ballGame::Result is not valid. Errors: game_id is not present."
     end
 
     it "provides timestamps on create and update" do
-      result = Result.create result_set: @content, game: @game
+      result = Result.create result_set: @result_set, game: @game
 
       expect(result.created_at).to be_kind_of Time
       expect(result.updated_at).to be_kind_of Time
