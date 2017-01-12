aa60807a99fb170ca723009e2edf2f24d3bcbc8a
stephan <railsc0d0r@gmail.com>
2017-01-12 01:36:27 +0100

    Refactors spec to DRY it up. Describes validation of config. Describes Game to provide config as json, too. Refs #66, #92, #162

--- spec/models/global8ball_game/game_spec.rb
+++ spec/models/global8ball_game/game_spec.rb
@@ -5,13 +5,12 @@ module Global8ballGame
     before do
       object_creator = ObjectCreator.new
       @config = object_creator.create_table_config
+      @game = Game.create config: @config
     end
 
     it "provides timestamps on create and update" do
-      game = Game.create
-
-      expect(game.created_at).to be_kind_of Time
-      expect(game.updated_at).to be_kind_of Time
+      expect(@game.created_at).to be_kind_of Time
+      expect(@game.updated_at).to be_kind_of Time
     end
 
     # it "includes Global8ballGame::PhysicsConcern" do
@@ -19,9 +18,15 @@ module Global8ballGame
     # end
 
     it "provides config as attribute to store a hash" do
-      Game.create config: @config
-
       expect(Game.all.first.config).to eq @config.deep_stringify_keys
+    end
+
+    it "validates presence of :config" do
+      expect {Game.create}.to raise_error "Global8ballGame::Game is not valid. Errors: config is not present."
+    end
+
+    it "provides config as json, too" do
+      expect(Game.all.first.config_json).to eq @config.to_json
     end
   end
 end
