a197539f25d1ca05a705e0e7ef6d1ce4912c9ab2
stephan <railsc0d0r@gmail.com>
2017-01-15 22:20:24 +0100

    Fixes games_controller in dummy-app. Refs #66, #92, #162

--- test/dummy/app/controllers/games_controller.rb
+++ test/dummy/app/controllers/games_controller.rb
@@ -1,13 +1,22 @@
 class GamesController < ApplicationController
   # GET /games
   def show
-    @game = Game.first || Game.create!
+    @game = Global8ballGame::Game.all.first
+
+    if @game.nil?
+      players = Player.all
+      player_1 = players[0]
+      player_2 = players[1]
+
+      @game = Global8ballGame::Game.create( player_1_id: player_1.id, player_1_name: player_1.name, player_2_id: player_2.id, player_2_name: player_2.name )
+    end
+
     render 'show', layout: false
   end
 
   def reset
-    @game = Game.first || Game.create!
-    @game.destroy
+    @game = Global8ballGame::Game.all.first
+    @game.delete unless @game.nil?
 
     redirect_to root_path
   end
