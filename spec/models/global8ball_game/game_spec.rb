9aed5f8ad7866e0af149c3e0bf23a90263ae5059
stephan <railsc0d0r@gmail.com>
2017-01-15 03:25:58 +0100

    Specifies Global8ball::Game to generate a new config before create. Refs #66, #92, #162

--- spec/models/global8ball_game/game_spec.rb
+++ spec/models/global8ball_game/game_spec.rb
@@ -7,7 +7,7 @@ module Global8ballGame
       @config = object_creator.create_table_config
       @player_1 = object_creator.players[:player_1]
       @player_2 = object_creator.players[:player_2]
-      @game = Game.create config: @config, player_1_id: @player_1.id, player_1_name: @player_1.name, player_2_id: @player_2.id, player_2_name: @player_2.name
+      @game = Game.create player_1_id: @player_1.id, player_1_name: @player_1.name, player_2_id: @player_2.id, player_2_name: @player_2.name
     end
 
     it "provides timestamps on create and update" do
@@ -20,7 +20,16 @@ module Global8ballGame
     end
 
     it "provides config as attribute to store a hash" do
-      expect(Game.all.first.config).to eq @config.deep_stringify_keys
+      expect(Game.all.first.config).to be_kind_of Hash
+    end
+
+    it "generates a new config before create" do
+      result_config = Game.all.first.config
+      damping = result_config['table'].delete('damping')
+      @config[:table].delete(:damping)
+
+      expect(0.12...0.22).to cover(damping)
+      expect(result_config).to eq @config.deep_stringify_keys
     end
 
     it "has a collection of results" do
@@ -43,7 +52,7 @@ module Global8ballGame
     end
 
     it "validates presence of player_1_id" do
-      expect {Game.create config: @config, player_1_name: @player_1.name, player_2_id: @player_2.id, player_2_name: @player_2.name}.to raise_error "Global8ballGame::Game is not valid. Errors: player_1_id is not present."
+      expect {Game.create player_1_name: @player_1.name, player_2_id: @player_2.id, player_2_name: @player_2.name}.to raise_error "Global8ballGame::Game is not valid. Errors: player_1_id is not present."
     end
 
     it "stores name of player 1" do
@@ -51,7 +60,7 @@ module Global8ballGame
     end
 
     it "validates presence of player_1_name" do
-      expect {Game.create config: @config, player_1_id: @player_1.id, player_2_id: @player_2.id, player_2_name: @player_2.name}.to raise_error "Global8ballGame::Game is not valid. Errors: player_1_name is not present."
+      expect {Game.create player_1_id: @player_1.id, player_2_id: @player_2.id, player_2_name: @player_2.name}.to raise_error "Global8ballGame::Game is not valid. Errors: player_1_name is not present."
     end
 
     it "stores player 2 as id" do
@@ -59,7 +68,7 @@ module Global8ballGame
     end
 
     it "validates presence of player_2_id" do
-      expect {Game.create config: @config, player_1_id: @player_1.id, player_1_name: @player_1.name, player_2_name: @player_2.name}.to raise_error "Global8ballGame::Game is not valid. Errors: player_2_id is not present."
+      expect {Game.create player_1_id: @player_1.id, player_1_name: @player_1.name, player_2_name: @player_2.name}.to raise_error "Global8ballGame::Game is not valid. Errors: player_2_id is not present."
     end
 
     it "stores name of player 2" do
@@ -67,7 +76,7 @@ module Global8ballGame
     end
 
     it "validates presence of player_2_name" do
-      expect {Game.create config: @config, player_1_id: @player_1.id, player_1_name: @player_1.name, player_2_id: @player_2.id}.to raise_error "Global8ballGame::Game is not valid. Errors: player_2_name is not present."
+      expect {Game.create player_1_id: @player_1.id, player_1_name: @player_1.name, player_2_id: @player_2.id}.to raise_error "Global8ballGame::Game is not valid. Errors: player_2_name is not present."
     end
   end
 end
