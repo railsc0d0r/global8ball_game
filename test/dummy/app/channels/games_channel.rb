f98d94f0234adb63be60a4ef60ea5b43ee755fc1
stephan <railsc0d0r@gmail.com>
2017-01-15 22:21:15 +0100

    Fixes games_channel in dummy-app. Refs #66, #92, #162

--- test/dummy/app/channels/games_channel.rb
+++ test/dummy/app/channels/games_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class GamesChannel < ApplicationCable::Channel
   def subscribed
-    @game = Game.find(params[:game_id])
+    @game = Global8ballGame::Game[params[:game_id]]
     transmit @game.config
   end
 
