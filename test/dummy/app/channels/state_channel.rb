f92b82939262e91c7724be16d5149c88d71f6103
stephan <railsc0d0r@gmail.com>
2017-01-15 22:21:41 +0100

    Fixes state_channel in dummy-app. Refs #66, #92, #162

--- test/dummy/app/channels/state_channel.rb
+++ test/dummy/app/channels/state_channel.rb
@@ -2,7 +2,7 @@
 class StateChannel < ApplicationCable::Channel
   def subscribed
     stream_from "state_#{params[:game_id]}"
-    @game = Game.find(params[:game_id])
+    @game = Global8ballGame::Game[params[:game_id]]
   end
 
   def unsubscribed
